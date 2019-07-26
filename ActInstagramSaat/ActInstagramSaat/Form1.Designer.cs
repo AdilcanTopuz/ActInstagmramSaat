bf71deaac0c06711c587c78ba18a137e4b78410f
Adilcan Topuz <adilcann18@gmail.com>
2019-07-26 14:33:31 +0300

    Add files via upload

---
+++ ActInstagramSaat/ActInstagramSaat/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ActInstagramSaat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.pbPP = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDogrulama = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(105, 14);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(185, 23);
+            this.txtKullaniciAdi.TabIndex = 1;
+            // 
+            // txtParola
+            // 
+            this.txtParola.Location = new System.Drawing.Point(105, 43);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.PasswordChar = '*';
+            this.txtParola.Size = new System.Drawing.Size(185, 23);
+            this.txtParola.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Parola:";
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBaslat.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.ForeColor = System.Drawing.Color.Green;
+            this.btnBaslat.Location = new System.Drawing.Point(12, 72);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(278, 50);
+            this.btnBaslat.TabIndex = 4;
+            this.btnBaslat.Text = "Giriş yap ve İşlemi başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // pbPP
+            // 
+            this.pbPP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPP.Location = new System.Drawing.Point(296, 14);
+            this.pbPP.Name = "pbPP";
+            this.pbPP.Size = new System.Drawing.Size(108, 108);
+            this.pbPP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPP.TabIndex = 5;
+            this.pbPP.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fb: 18adilcan";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(138, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IG: 18adilcan";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label5.Location = new System.Drawing.Point(264, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "adilcantopuz.com.tr";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // txtDogrulama
+            // 
+            this.txtDogrulama.Location = new System.Drawing.Point(156, 186);
+            this.txtDogrulama.Name = "txtDogrulama";
+            this.txtDogrulama.PasswordChar = '*';
+            this.txtDogrulama.Size = new System.Drawing.Size(185, 23);
+            this.txtDogrulama.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Doğrulama Kodu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(413, 161);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDogrulama);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbPP);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ACT Instagram Saat Profil Fotoğrafı";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.PictureBox pbPP;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDogrulama;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ActInstagramSaat/ActInstagramSaat/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InstagramApiSharp;
+using InstagramApiSharp.API;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Logger;
+using System.IO;
+
+namespace ActInstagramSaat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private static IInstaApi api;
+        private static UserSessionData user;
+        private string zaman = "";
+        private Bitmap bmp;
+        private async void btnBaslat_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (string.IsNullOrEmpty(txtKullaniciAdi.Text))
+                {
+                    MessageBox.Show("Kullanıcı Adı Boş Olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(txtParola.Text))
+                {
+                    MessageBox.Show("Parola Boş Olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                user = new UserSessionData();
+                user.UserName = txtKullaniciAdi.Text;
+                user.Password = txtParola.Text;
+                api = InstaApiBuilder.CreateBuilder().SetUser(user).UseLogger(new DebugLogger(LogLevel.Exceptions)).Build();
+
+                var logInResult = await api.LoginAsync();
+                if (logInResult.Succeeded)
+                {
+                    timer1.Start();
+                }
+                else
+                {
+                    if (logInResult.Value == InstaLoginResult.ChallengeRequired)
+                    {
+                        var challenge = await api.GetChallengeRequireVerifyMethodAsync();
+                        if (challenge.Succeeded)
+                        {
+                            if (challenge.Value.SubmitPhoneRequired)
+                            {
+                                //telefon
+                            }
+                            else
+                            {
+                                if (challenge.Value.StepData != null)
+                                {
+                                    if (!string.IsNullOrEmpty(challenge.Value.StepData.PhoneNumber))
+                                    {
+                                        var pnv = challenge.Value.StepData.PhoneNumber;
+                                        MessageBox.Show(pnv);
+                                        sendCode(api);
+                                    }
+                                    if (!string.IsNullOrEmpty(challenge.Value.StepData.Email))
+                                    {
+                                        var emv = challenge.Value.StepData.Email;
+                                        MessageBox.Show(emv);
+                                        sendCode(api);
+                                    }
+                                    
+                                }
+                            }
+                        }
+                        else
+                            MessageBox.Show(challenge.Info.Message, "ERR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        async void sendCode(IInstaApi api)
+        {
+            bool isEmail = true;
+            try
+            {                
+                if (isEmail)
+                {
+                    var email = await api.RequestVerifyCodeToEmailForChallengeRequireAsync();
+                    this.Size = new Size(429, 260);
+                    if (email.Succeeded)
+                    {
+                        MessageBox.Show("gönderildi");
+                    }
+                    else
+                        MessageBox.Show(email.Info.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    var phoneNumber = await api.RequestVerifyCodeToSMSForChallengeRequireAsync();
+                    if (phoneNumber.Succeeded)
+                    {
+                        MessageBox.Show("doğrulandı");
+                        this.Size = new Size(429, 200);
+                    }
+                    else
+                        MessageBox.Show(phoneNumber.Info.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        public async void Dogrula(IInstaApi api)
+        {
+            try
+            {
+                var verifyLogin = await api.VerifyCodeForChallengeRequireAsync(txtDogrulama.Text);
+                if (verifyLogin.Succeeded)
+                {
+                    MessageBox.Show("doğrulandı");
+                    this.Size = new Size(429, 200);
+                    timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Yanlış kod");
+                    if (verifyLogin.Value == InstaLoginResult.TwoFactorRequired)
+                    {
+                        MessageBox.Show("iki faktörlü doğrulama gerekiyor.");
+                    }
+                    else
+                        MessageBox.Show(verifyLogin.Info.Message, "ERR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"D");
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (zaman != DateTime.Now.ToLongTimeString())
+                {
+                    zaman = DateTime.Now.ToLongTimeString();
+                    string dosyaYolu = "resim.png";
+                    bmp = new Bitmap(dosyaYolu);
+                    Graphics g = Graphics.FromImage(bmp);
+                    g.DrawString("@18adilcan", new Font("Century Gothic", Convert.ToInt32(60), FontStyle.Bold), new SolidBrush(Color.White), 220f, 650f);
+                    g.DrawString(DateTime.Now.ToLongTimeString(), new Font("Times New Roman", Convert.ToInt32(180), FontStyle.Bold), new SolidBrush(Color.White), 40f, 300f);
+                    pbPP.Image = bmp;
+                    bmp.Save("resimPP.png");
+                    string ppYolu = "resimPP.png";
+                    byte[] Bytes = File.ReadAllBytes(ppYolu);
+                    await api.AccountProcessor.ChangeProfilePictureAsync(Bytes);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Text = "Hata: " + ex.Message;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://facebook.com/18adilcan");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://instagram.com/18adilcan");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("https://adilcantopuz.com.tr");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dogrula(api);
+        }
+    }
+}
